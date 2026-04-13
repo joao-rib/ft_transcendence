4519708efd1498bf00e055b2152a212a81c92ac3
jose5556 <josefilmeneses@gmail.com>
2026-04-13 09:49:22 +0100

    refactor: adjust formatting and indentation in Pieces component

--- app/components/chess_game/Pieces/Pieces.js
+++ app/components/chess_game/Pieces/Pieces.js
@@ -1,9 +1,9 @@
 import './Pieces.css'
 
 const Pieces = () => {
-const position = new Array(8).fill('').map(x => new Array(8).fill(''))
-console.log(position)
-return <div className='pieces'></div>
+	const position = new Array(8).fill('').map(x => new Array(8).fill(''))
+	console.log(position)
+	//return <div className='pieces'></div>
 }
 
 export default Pieces
