f30bf30e41e06e07717d9b7112b58b7048aab098
jose5556 <josefilmeneses@gmail.com>
2026-04-13 12:55:45 +0100

    feat: implement piece drag-and-drop functionality and initialize board state

--- app/components/chess_game/Pieces/Piece.js
+++ app/components/chess_game/Pieces/Piece.js
@@ -4,11 +4,19 @@ const Piece = ({
     piece,
 }) => {
 
+	const onDragStart = e => {
+		e.dataTransfer.effectAllowed = "move";
+		e.dataTransfer.setData("text/plain",`${piece},${rank},${file}`)
+		setTimeout(() => {
+            e.target.style.display = 'none'
+        },0)
+	}
+
 	return (
 		<div 
 			className={`piece ${piece} pos-${file}${rank}`}
 			draggable={true}
-			
+			onDragStart={onDragStart}
 		/>
 	)
 }
