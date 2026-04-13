b64e10cf747dc2d8d094a6f08e25e8167be8f947
Whyflyy <cavadasribeiro.joao@gmail.com>
2026-04-13 13:47:44 +0100

    Small theme adjustment

---
+++ app/frontend/rankings/page.tsx
@@ -0,0 +1,17 @@
+import Background from "../components/Background";
+
+export default function Rules() {
+	return (
+		<div className="relative min-h-screen overflow-hidden font-sans">
+			<Background />
+
+
+			<main className="relative z-10 flex min-h-screen flex-col items-center justify-center px-8 py-20">
+				<div className="w-full max-w-4xl mx-auto space-y-12">
+					
+
+				</div>
+			</main>
+		</div>
+	);
+}
