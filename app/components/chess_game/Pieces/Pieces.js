a7bb04bd45ae400aeab38cd0a2dbf6096f5cb594
jose5556 <josefilmeneses@gmail.com>
2026-04-13 13:17:53 +0100

    feat: enhance Pieces component with drag-and-drop functionality and coordinate calculation

--- app/components/chess_game/Pieces/Pieces.js
+++ app/components/chess_game/Pieces/Pieces.js
@@ -2,18 +2,34 @@
 
 import './Pieces.css'
 import Piece from './Piece'
-import { useState } from 'react'
-import { createPosition } from '../helper'
+import { useState, useRef } from 'react'
+import { createPosition, copyPosition } from '../helper'
 
 const Pieces = () => {
 
+	const ref = useRef()
 	const [state,setState] = useState(createPosition())
+
+	 const calculateCoords = e => {
+        const {top,left,width} = ref.current.getBoundingClientRect()
+        const size = width / 8
+        const y = Math.floor((e.clientX - left) / size) 
+        const x = 7 - Math.floor((e.clientY - top) / size)
+
+        return {x,y}
+    }
+
 	const onDrop = e => {
-		console.log(e.dataTransfer.getData('text'));
+		const newPostion = copyPosition(state)
+		const {x,y} = calculateCoords(e)
+		const [p, rank,file] = e.dataTransfer.getData('text').split(',')
+		newPosition[rank][file] = ''
+		newPosition[x][y] = p
 	}
 	const onDragOver = e => e.preventDefault()
 
 	return <div
+		ref = {ref}
 		onDrop={onDrop}
 		onDragOver={onDragOver}
 		className='pieces'>
