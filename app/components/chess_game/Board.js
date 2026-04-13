e87491eb6590985b0ded04dc5d9e4f3c7bfb43f8
jose5556 <josefilmeneses@gmail.com>
2026-04-13 09:43:48 +0100

    feat: add Pieces component and integrate into Board

--- app/components/chess_game/Board.js
+++ app/components/chess_game/Board.js
@@ -2,6 +2,7 @@ import { getCharacter } from '../../helper'
 import './Board.css'
 import Files from './bits/Files'
 import Ranks from './bits/Ranks'
+import Pieces from './Pieces/Pieces'
 
 const Board = () => {
 
@@ -10,22 +11,26 @@ const Board = () => {
 		c+= (i+j) % 2 === 0 ? ' tile--dark' : ' tile--light'
 		return c
 	}
-	
+
 	const ranks = Array(8).fill().map((x,i) => 8 - i)
 	const files = Array(8).fill().map((x,i) => i + 1)
 
 	return <div className='board'>
-
 
 		<Ranks ranks={ranks}/>
 
 		<div className='tiles'>
 			{ranks.map((rank,i) =>
 				files.map((file,j) =>
-					<div key={file+'-'+rank} className={getClassName(9 - i,j)}></div>
+					<div 
+						key={file+''+rank} 
+						className={getClassName(9 - i,j)}>
+					</div>
 				)
 			)}
 		</div>
+
+		<Pieces/>
 
 		<Files files={files}/>
 	</div>
