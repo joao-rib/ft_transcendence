e026099b18e9f8da9b503710278fb9d3fc2d46bb
jose5556 <josefilmeneses@gmail.com>
2026-04-14 19:28:06 +0100

    feat: implement getKnightMoves and getBishopMoves functions for chess piece movement

--- app/components/chess_game/arbiter/arbiter.js
+++ app/components/chess_game/arbiter/arbiter.js
@@ -1,8 +1,14 @@
-import { getRookMoves } from './getMoves'
+import { getBishopMoves, getKnightMoves, getRookMoves } from './getMoves'
 
 const arbiter = {
+
 	getRegularMoves : function({position, piece, rank, file}) {
-		return getRookMoves({position, piece, rank, file})
+		if (piece.endsWith('n'))
+			return getKnightMoves({position, rank, file})
+		if (piece.endsWith('r'))
+			return getRookMoves({position, piece, rank, file})
+		if (piece.endsWith('b'))
+			return getBishopMoves({position, piece, rank, file})
 	}
 }
 
