b4b3660754cfd272fb9f0837c08c57cc31b3f32e
jose5556 <josefilmeneses@gmail.com>
2026-04-22 09:37:59 +0100

    feat: add Resign button component and integrate into GamePage

---
+++ app/components/chess_game/Control/bits/Resign.js
@@ -0,0 +1,16 @@
+const Resign = () => {
+  const handleResign = () => {
+    // Placeholder action until resign game flow is implemented.
+    console.log('Resign clicked')
+  }
+
+  return (
+    <div>
+      <button className='resign-btn' onClick={handleResign}>
+        Resign
+      </button>
+    </div>
+  )
+}
+
+export default Resign
--- app/game/page.tsx
+++ app/game/page.tsx
@@ -6,6 +6,7 @@ import CompactChat from '@/app/components/compact-chat'
 import { initGameState } from '../components/chess_game/constants'
 import Control from '../components/chess_game/Control/Control'
 import TakeBack from '../components/chess_game/Control/bits/TakeBack'
+import Resign from '../components/chess_game/Control/bits/Resign'
 import { reducer } from '../components/chess_game/reducer/reducer'
 import AppContext from '../contexts/Context'
 import { useCrossTabGameSync } from './useCrossTabGameSync'
@@ -26,6 +27,7 @@ export default function GamePage() {
         <Board />
         <Control>
           <TakeBack />
+          <Resign />
           <div style={{ marginTop: '12px', minWidth: '260px' }}>
             <CompactChat maxHeight='max-h-80' />
           </div>
