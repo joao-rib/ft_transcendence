60a79a49b8ca64f92ce88303638e512c0dca9065
joao-rib <jonnasn@gmail.com>
2026-04-23 22:56:27 +0100

    importing some rankings

---
+++ app/frontend/rankings/components/RankingsLoading.tsx
@@ -0,0 +1,7 @@
+export default function RankingsLoading() {
+	return (
+		<div className="text-center" style={{ color: "var(--text-secondary)" }}>
+			<p className="text-lg">Loading leaderboard...</p>
+		</div>
+	);
+}
